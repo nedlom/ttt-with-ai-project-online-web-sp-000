0d6af40fcf3933a3567684f16b2c3fdecbf060af
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-02 20:07:38 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,6 +7,13 @@ module Players
     def move(board)
       
       opponent_token = Player.all.detect {|p| p != self}.token
+      
+      near_win = Game.wins.detect do |win|
+        winning_cells = [board.cells[win[0]], board.cells[win[1]], board.cells[win[2]]]
+        winning_cells.count(opponent_token) == 2 && winning_cells.count(" ") == 1
+      end   
+      
+      
       binding.pry
       
       # computer = self.token
@@ -18,31 +25,31 @@ module Players
       # if player is near win
       # returns that combo
       # and assigns it to variable x
-      x = Game.wins.detect do |win|
+      # near_win = Game.wins.detect do |win|
         #these are the winning indices in the array
         # a = win[0]
         # b = win[1]
         # c = win[2]
         # board.cells is the game_board_array 
         #d is the sub_array of the game_board_array
-        winning_cells = [board.cells[win[0]], board.cells[win[1]], board.cells[win[2]]]
-        d.count(opponent_token) == 2 && d.count(" ") == 1
-      end
+      #   winning_cells = [board.cells[win[0]], board.cells[win[1]], board.cells[win[2]]]
+      #   winning_cells.count(opponent_token) == 2 && winning_cells.count(" ") == 1
+      # end
       
       # binding.pry
       
       #detects an open corner on the TTT board
       open_corner = [0, 2, 6, 8].detect {|i| board.cells[i] == " "}
       
-      if !x.nil?
-        x.detect {|i| board.cells[i] == " "} + 1
-      elsif board.cells[4] == " "
-        5.to_s
-      elsif !y.nil?
-        y + 1
-      else
-        rand(1..9)
-      end
+      # if !x.nil?
+      #   x.detect {|i| board.cells[i] == " "} + 1
+      # elsif board.cells[4] == " "
+      #   5.to_s
+      # elsif !y.nil?
+      #   y + 1
+      # else
+      #   rand(1..9)
+      # end
     end
   end
 end
